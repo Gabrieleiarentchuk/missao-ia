9ca9ff8c62ffdf57bb38f6bac9550a8cd176fbb9
Gabrieleiarentchuk <140639706+Gabrieleiarentchuk@users.noreply.github.com>
2025-11-05 14:30:06 -0300

    123456789

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,13 +1,97 @@
 export const perguntas = [
     {
-    enunciado:"",
+    enunciado:"A inserção de dispositivos digitais e telas (como smartphones, tablets e televisões) na rotina de crianças em idade pré-escolar e escolar é um fenômeno global que suscita preocupações e oportunidades. Considerando o desenvolvimento infantil integral, qual dos dilemas a seguir melhor representa os impactos divergentes do uso de telas?",
     alternativas:[
         {
-            texto: "",
+            texto: "O uso moderado e supervisionado de telas serve como um poderoso instrumento de enriquecimento pedagógico.",
+            afirmacao:[
+                "."
+            ],
+            proxima: 1,
+        },
+        {
+            texto:"",
             afirmacao:[
                 ""
             ],
+            proxima: 2,
         }
     ]
-}
+},
+{
+    enunciado:"",
+    alternativas:[
+        {
+            texto:"",
+            afirmacao:[
+                  ""
+            ],
+            proxima:3,
+        },
+        {
+            texto:"",
+            afirmacao: [
+                  ""
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao: [
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao: [
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
+{
+    enunciado:
+    alternativas:[
+        {
+            texto:
+            afirmacao:[
+
+            ],
+            proxima:3,
+        },
+        {
+            texto:
+            afirmacao: [
+
+            ],
+            proxima: 4,
+        }
+    ]
+},
 ]
