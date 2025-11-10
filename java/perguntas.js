6b57577c1bd16591e364e12b8efba2bb3810fe5b
Gabrieleiarentchuk <140639706+Gabrieleiarentchuk@users.noreply.github.com>
2025-11-10 09:23:31 -0300

    12345678

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,9 +1,9 @@
 export const perguntas = [
     {
-    enunciado:"A inserção de dispositivos digitais e telas (como smartphones, tablets e televisões) na rotina de crianças em idade pré-escolar e escolar é um fenômeno global que suscita preocupações e oportunidades. Considerando o desenvolvimento infantil integral, qual dos dilemas a seguir melhor representa os impactos divergentes do uso de telas?",
+    enunciado:"",
     alternativas:[
         {
-            texto: "O uso moderado e supervisionado de telas serve como um poderoso instrumento de enriquecimento pedagógico.",
+            texto: "",
             afirmacao:[
                 "."
             ],
